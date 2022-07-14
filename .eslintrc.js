05f8733685d49f41681b4dadf3e07a63f8c02b30
Kelsi Lane <kelsilane91@gmail.com>
2022-07-13 21:35:10 -0500

    chore: environment setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
+
   extends: '@react-native-community',
+
+  plugins: ['@typescript-eslint'],
+
+  // TS specific linting
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+
+      parser: '@typescript-eslint/parser',
+
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 0,
+      },
+    },
+  ],
 };
