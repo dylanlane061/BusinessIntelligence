c15083f8afe361c75740eae9bd38b727206de0e7
dylanlane061 <dylanlane061@gmail.com>
2022-07-18 09:56:38 -0500

    feat: add company item

--- src/components/CompanyItem.tsx
+++ src/components/CompanyItem.tsx
@@ -1,24 +1,75 @@
-import React, {useCallback} from 'react';
+import React, {memo} from 'react';
+import {
+  Pressable,
+  PressableStateCallbackType,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {Company, NavParamMap} from '../types';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {Avatar} from './Avatar';
 
-export const CompanyItem = (props: Company) => {
-  const {name} = props;
+const styles = StyleSheet.create({
+  itemRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  infoRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoColumn: {
+    marginLeft: 8,
+  },
+  pressedStyle: {
+    opacity: 0.5,
+  },
+});
+
+type CompanyItemProps = {
+  company: Company;
+  style?: StyleProp<ViewStyle>;
+};
+
+export const propsAreEqual = (
+  prev: CompanyItemProps,
+  next: CompanyItemProps,
+) => {
+  return prev.style === next.style && prev.company.id === next.company.id;
+};
+
+export const CompanyItem = memo((props: CompanyItemProps) => {
+  const {company, style} = props;
+
   const {navigate} = useNavigation<NavigationProp<NavParamMap>>();
 
+  const determineStyle = ({pressed}: PressableStateCallbackType) => [
+    styles.itemRow,
+    style,
+    pressed && styles.pressedStyle,
+  ];
+
+  const onPress = () => navigate('Profile', company);
+
   return (
-    <TouchableOpacity
-      onPress={useCallback(() => navigate('Profile', props), [])}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          padding: 16,
-        }}>
-        <Text>{name}</Text>
-        <Text>{'>'}</Text>
+    <Pressable style={determineStyle} onPress={onPress}>
+      <View style={styles.infoRow}>
+        <Avatar name={company.name} size={40} />
+
+        <View style={styles.infoColumn}>
+          <Text>{company.name}</Text>
+          <Text>
+            {company.location.address}, {company.location.city}
+          </Text>
+        </View>
       </View>
-    </TouchableOpacity>
+
+      <Text>{'>'}</Text>
+    </Pressable>
   );
-};
+}, propsAreEqual);
