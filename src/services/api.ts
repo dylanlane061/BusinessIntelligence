5f36d7f1e5145177b328c4d8e8b356b438d52e06
dylanlane061 <dylanlane061@gmail.com>
2022-07-18 09:37:27 -0500

    refactor: create services folder

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,10 @@
 import data from './data.json';
-import {Company} from './types';
+import {Company} from '../types';
 
 export const delay = (time: number) =>
   new Promise(resolve => setTimeout(resolve, time));
 
 export const getCompanies = async () => {
-  await delay(5000);
+  await delay(2000);
   return data as Company[];
-};
-
-export const getCompany = async (id: number) => {
-  await delay(5000);
-  return data.find(company => company.id === id);
 };
